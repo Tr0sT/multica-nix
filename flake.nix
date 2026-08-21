371033ca01a82b0f679eb2d1a966ceb062b95219
Sergey Morozov <tr0st@bk.ru>
2026-08-21 16:18:30 +0300

    chore: update multica to 0.4.32 (#50)
    Co-authored-by: Tr0sT <3677938+Tr0sT@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
       ];
       forAllSystems = nixpkgs.lib.genAttrs systems;
       pkgsFor = system: import nixpkgs { inherit system; };
-      version = "0.4.31";
+      version = "0.4.32";
     in
     {
       packages = forAllSystems (
